d3b055762f48843d7cac439fbf9c8a02b7452546
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-20 11:18:26 -0400

    user put route/ update password feature added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,7 +49,23 @@ router.post('/', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'no user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
 
 router.delete('/:id', (req, res) => {
