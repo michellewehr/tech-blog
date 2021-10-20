d846cb63611d0076c3e4d3086775fd61f7bf6489
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-20 19:20:21 -0400

    homepage now shows post info

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,32 @@
+const { Post, User } = require('../models');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_body',
+            'created_at', 
+            'user_id'
+        ],
+        include: {
+            model: User,
+            attributes: ['username']
+        }
+    })
+  .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+    console.log(posts);
+      res.render('homepage', {
+          posts
+      })
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  })
 });
 
 module.exports = router;
